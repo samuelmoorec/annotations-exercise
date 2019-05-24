63b88ad86f76832a576d536466e3d4c2fc6fb16c
arleneamericamartinez <hubacek1221>
2019-05-24 12:18:11 -0500

    I think i did this right

--- src/Author.java
+++ src/Author.java
@@ -1,9 +1,7 @@
-/**
- * This source file is subject to the license that is bundled with this package in the file LICENSE.
- */
+
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings({"unchecked","WeakerAccess"})
 public class Author extends Person {
     private List books;
 
